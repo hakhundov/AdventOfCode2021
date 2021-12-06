bf1fbe2bae97e96c7eca048db4af92c19514f340
haji-akhundov <haji.akhundov@pwc.com>
2021-12-06 15:54:13 +0100

    d5p2: cleanup

--- day5/main_p2.py
+++ day5/main_p2.py
@@ -1,3 +1,5 @@
+SKIP_DIAGONALS = False
+
 def update_point(point):
     if point in points:
         points[point] += 1
@@ -5,15 +7,15 @@ def update_point(point):
         points[point] = 1
 
 
-def process_line(line):
-    x, y = int(line[0]), int(line[1])
+def process_line(x1, y1, x2, y2):
+    x, y = x1, y1
     update_point(f"{x},{y}")
-    kx = -1 if int(line[0]) > int(line[2]) else 1
-    ky = -1 if int(line[1]) > int(line[3]) else 1
+    kx = -1 if x1 > x2 else 1
+    ky = -1 if y1 > y2 else 1
 
-    while x != int(line[2]) or y != int(line[3]):
-        x = x + (x != int(line[2])) * kx
-        y = y + (y != int(line[3])) * ky
+    while x != x2 or y != y2:
+        x = x + (x != x2) * kx
+        y = y + (y != y2) * ky
         update_point(f"{x},{y}")
 
 
@@ -21,16 +23,15 @@ points = {}
 count = 0
 
 
-with open('input') as f:
-    for line in f:
-        s = line.strip().split(" -> ")
-        x1, y1, x2, y2 = s[0].split(",") + s[1].split(",")
-        process_line([x1, y1, x2, y2])
+with open('test_input') as file:
+    for input_line in file:
+        s = input_line.strip().split(" -> ")
+        x1, y1 = [int(x) for x in s[0].split(",")]
+        x2, y2 = [int(x) for x in s[1].split(",")]
+        process_line(x1, y1, x2, y2)
 
 
 for k in points.values():
         count += (k > 1)
 
 print(count)
-# h_v_lines = [end_points(line) for line in lines if is_diagonal(end_points(line))]
-#all_lines = [end_points(line) for line in lines]
