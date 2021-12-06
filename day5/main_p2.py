e4785d44719c58b3cef7b0754e3fcde0bffd0986
haji-akhundov <haji.akhundov@pwc.com>
2021-12-06 15:19:43 +0100

    refactor code

--- day5/main_p2.py
+++ day5/main_p2.py
@@ -1,19 +1,4 @@
-# from matplotlib import pyplot as plt
-
-with open('input') as f: input_lines = f.read()
-
-lines = input_lines.split('\n')
-
-
-def end_points(raw_end_points):
-    s = raw_end_points.replace(" ", "").split("->")
-    return s[0].split(",") + s[1].split(",")
-
-
-def update_points(point):
-    # x = int(point.split(",")[0])
-    # y = int(point.split(",")[1])
-    # plt.scatter(x,y)
+def update_point(point):
     if point in points:
         points[point] += 1
     else:
@@ -22,25 +7,30 @@ def update_points(point):
 
 def process_line(line):
     x, y = int(line[0]), int(line[1])
-    update_points(f"{x},{y}")
+    update_point(f"{x},{y}")
     kx = -1 if int(line[0]) > int(line[2]) else 1
     ky = -1 if int(line[1]) > int(line[3]) else 1
 
     while x != int(line[2]) or y != int(line[3]):
         x = x + (x != int(line[2])) * kx
         y = y + (y != int(line[3])) * ky
-        update_points(f"{x},{y}")
+        update_point(f"{x},{y}")
 
-# h_v_lines = [end_points(line) for line in lines if is_diagonal(end_points(line))]
-all_lines = [end_points(line) for line in lines]
 
 points = {}
-
-for l in all_lines:
-    process_line(l)
-
 count = 0
+
+
+with open('input') as f:
+    for line in f:
+        s = line.strip().split(" -> ")
+        x1, y1, x2, y2 = s[0].split(",") + s[1].split(",")
+        process_line([x1, y1, x2, y2])
+
+
 for k in points.values():
         count += (k > 1)
 
 print(count)
+# h_v_lines = [end_points(line) for line in lines if is_diagonal(end_points(line))]
+#all_lines = [end_points(line) for line in lines]
