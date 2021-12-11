012539f286ce2703e1066b3cfe80f3e61d4d84f4
haji-akhundov <haji.akhundov@pwc.com>
2021-12-11 14:18:52 +0400

    d7p2 modified for part2

--- day7/main.py
+++ day7/main.py
@@ -3,12 +3,16 @@
 with open('input') as file:
     initial_position = [int(x) for x in file.readline().split(',')]
 
-maximum = max(initial_position)
-fuel_consumption = [0 for x in range(0, maximum+1, 1)]
+maximum_position = max(initial_position)
+fuel_consumption = [0 for x in range(0, maximum_position + 1, 1)]
 
-for x in range(0, maximum+1, 1):
+for x in range(0, maximum_position + 1, 1):
     fuel_consumption[x] = 0
     for i in initial_position:
-        fuel_consumption[x] += abs(int(i) - x)
+        delta = abs(int(i) - x)
+        # part 1
+        # fuel_consumption[x] += delta
+        # part 2
+        fuel_consumption[x] += sum(range(1, delta + 1, 1))
 
 print(min(fuel_consumption))
