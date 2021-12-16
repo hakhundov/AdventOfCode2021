bc2ae99b38aeaed30f1c96609e21cd183ec2b927
haji-akhundov <haji.akhundov@pwc.com>
2021-12-17 00:51:47 +0400

    d9 p2 solved

--- day9/main.py
+++ day9/main.py
@@ -1,4 +1,7 @@
-with open('test_input') as file:
+import queue
+import math
+
+with open('input') as file:
     input_lines = file.readlines()
 
 y_max = len(input_lines) + 2
@@ -15,13 +18,43 @@ low_points = []
 risk_level = 0
 for y in range(1, y_max - 1, 1):
     for x in range(1, x_max - 1, 1):
-        if      ARR[y][x] < ARR[y][x - 1] and \
+        if ARR[y][x] < ARR[y][x - 1] and \
                 ARR[y][x] < ARR[y][x + 1] and \
                 ARR[y][x] < ARR[y - 1][x] and \
                 ARR[y][x] < ARR[y + 1][x]:
             risk_level += int(ARR[y][x]) + 1
             print(ARR[y][x])
-            low_points.append([x, y]) #x, y
+            low_points.append([y, x])
 
-print(low_points)
 print(risk_level)
+
+
+# part 2
+def get_neighbors(cx, cy):
+    return [[cx - 1, cy], [cx + 1, cy], [cx, cy - 1], [cx, cy + 1]]
+
+
+frontier = queue.Queue()
+reached = []
+
+
+def count_bredth(seed):
+    frontier.put(seed)
+    reached.append(seed)
+    count = 0
+    while not frontier.empty():
+        current = frontier.get()
+        count += 1
+        for next in get_neighbors(current[0], current[1]):
+            if next not in reached and int(ARR[next[0]][next[1]]) != 9:
+                frontier.put(next)
+                reached.append(next)
+    return count
+
+depth_counts = []
+for point in low_points:
+    c = count_bredth(point)
+    depth_counts.append(c)
+
+depth_counts.sort(reverse=True)
+print(math.prod(depth_counts[0:3]))
