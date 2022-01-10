722bff69f093cf8a1e0c89665205197aee85bd00
haji-akhundov <haji.akhundov@pwc.com>
2022-01-10 17:12:48 +0100

    day 14 cleanup

--- day14/main.py
+++ day14/main.py
@@ -1,38 +1,37 @@
+#TODO: Note: Part 2 doesn't work. (takes too long, fix it)
+
 from collections import Counter
 import time
 
 start_time = time.time()
 
-with open('input') as f: input_lines = f.read()
+with open('test_input') as f: input_lines = f.read()
 
 input = input_lines.split('\n\n')
-polymer = list(input[0])
+initial_polymer = list(input[0])
 instructions = [x.split(' -> ') for x in input[1].split('\n')]
 d = {}
 [d.update({k:v}) for k,v in instructions]
 
-copy = polymer.copy()
+polymer = initial_polymer.copy()
 
-steps = 15
+steps = 10
 
 for i in range(steps):
     new_poly = []
-    for k in range(0, len(copy)-1, 1):
-        str = ''.join(copy[k:k+2])
+    for k in range(0, len(polymer) - 1, 1):
+        str = ''.join(polymer[k:k + 2])
         if str in d.keys():
-            polymer.insert(k+1, d[str])
-            new_poly.append(copy[k])
+            new_poly.append(polymer[k])
             new_poly.append(d[str])
         else:
-            new_poly.append(copy[k])
-    new_poly.append(copy[len(copy)-1])
-    copy = new_poly
-    # print(f'Step {i}: Polymer: {new_poly}')
+            new_poly.append(polymer[k])
+    new_poly.append(polymer[len(polymer) - 1])
+    polymer = new_poly
+    # print(f'Step {i}: Polymer: {len(new_poly)}')
 
-# print(new_poly)
-# print(copy)
 print("--- %s seconds ---" % (time.time() - start_time))
 
-c = list(Counter(copy).values())
+c = list(Counter(polymer).values())
 c.sort()
 print(c[len(c)-1] - c[0])
