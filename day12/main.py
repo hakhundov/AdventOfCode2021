d74ca07297f749084a94f697020f220f680e1a8d
haji-akhundov <haji.akhundov@pwc.com>
2022-01-22 22:00:18 +0100

    Day 12, part 2

--- day12/main.py
+++ day12/main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+
 with open('input') as f: lines = f.readlines()
 G = defaultdict(set)
 
@@ -6,27 +7,43 @@ for l in lines:
     v1, v2 = l.strip().split('-')
     G[v1].add(v2)
 
-for k,val in G.copy().items():
+for k, val in G.copy().items():
     for v in val:
         G[v].add(k)
-# print(G)
 
 path = ['start']
-counter = 0
-def visit_node(vertex):
+
+small_caves = 0
+
+
+def visit_node(vertex, _allow_duplicates=False):
     global counter
+    global small_caves
+
     for node in G[vertex]:
         if node == 'end':
             counter += 1
         elif node.islower():
             if node not in path:
                 path.append(node)
-                visit_node(node)
+                visit_node(node, _allow_duplicates)
                 path.pop()
+            elif _allow_duplicates and small_caves == 0 and node != 'start':
+                small_caves = 1
+                path.append(node)
+                visit_node(node, _allow_duplicates)
+                path.pop()
+                small_caves = 0
         else:
             path.append(node)
-            visit_node(node)
+            visit_node(node, _allow_duplicates)
             path.pop()
 
-visit_node('start')
+counter = 0
+visit_node('start', _allow_duplicates=False)
 print(counter)
+assert (counter == 5104)
+counter = 0
+visit_node('start', _allow_duplicates=True)
+print(counter)
+assert (counter == 149220)
